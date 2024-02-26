c5e6050aeb87795f7467ce2a3e64f1a53bd5d2be
Chi-hung Wen <wen00033@algonquinlive.com>
2024-02-26 17:20:01 -0500

    Remove unused Redux store configuration

--- src/store.tsx
+++
@@ -1,8 +0,0 @@
-// create a redux store using configureStore from @reduxjs/toolkit
-// import { configureStore } from "@reduxjs/toolkit";
-
-// const store = configureStore({
-//   reducer: {},
-// });
-
-// export default store;
